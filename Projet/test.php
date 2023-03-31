d71565d31bb1b78e8f41df880e7ab12644ac86eb
Newball <newball875@gmail.com>
2023-03-31 15:30:03 +0200

    mise à jour du test

--- Projet/test.php
+++ Projet/test.php
@@ -1,4 +1,6 @@
 <?php
-
-?>
-SALUT
+$path=$_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR."Web3".DIRECTORY_SEPARATOR."Web3_Projet".DIRECTORY_SEPARATOR."Projet".DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."Autoloader.php";
+require $path;
+Autoloader::register();
+$pdo=connexion::connect();
+echo "Salut";
