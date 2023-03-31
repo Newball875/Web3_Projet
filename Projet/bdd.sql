2058db4890e04dabdea53d7a4cf25d2b36c2ff14
devisme valentin <vdevisme@poireauxl.kilimandjaro.calais.fr>
2023-03-31 15:55:09 +0200

    update bdd incomplete

--- Projet/bdd.sql
+++ Projet/bdd.sql
@@ -1,48 +1,59 @@
 
-
+DROP TABLE IF EXISTS recette;
 CREATE TABLE `recette` (
   `recette_id` varchar(50),
   `description` longtext,
   `image` varchar(255) DEFAULT NULL,
   `difficulte` varchar(20),
   `origine_id` varchar(50)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS ustensile;
 CREATE TABLE `ustensile` (
   `ustensile_id` varchar(50),
   `image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS ustensile_recette;
 CREATE TABLE `ustensile_recette` (
   `recette_id` varchar(50),
   `ustensile_id` varchar(50)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS ingredient;
 CREATE TABLE `ingredient` (
   `ingredient_id` varchar(50),
   `image` varchar(255) DEFAULT NULL,
   `type` varchar(25)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS ingredient_recette;
 CREATE TABLE `ingredient_recette` (
   `recette_id` varchar(50),
   `ingredient_id` varchar(50)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS tag;
 CREATE TABLE `tag` (
   `tag_id` varchar(50)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS tag_recette;
 CREATE TABLE `tag_recette` (
   `tag_id` varchar(50),
   `recette_id` varchar(50)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+DROP TABLE IF EXISTS origine;
 CREATE TABLE `origine` (
   `origine_id` varchar(50),
   `description` longtext,
   `image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
+
+
+ALTER TABLE `origine`
+  ADD CONSTRAINT `pk_origine` PRIMARY KEY (`origine_id`);
 
 ALTER TABLE `recette`
   ADD CONSTRAINT `pk_recette` PRIMARY KEY (`recette_id`);
@@ -54,7 +65,7 @@ ALTER TABLE `ustensile`
 ALTER TABLE `ustensile_recette`
   ADD CONSTRAINT `pk_ustensile_recette` PRIMARY KEY (`recette_id`,`ustensile_id`);
   ADD CONSTRAINT `fk_ustensile` FOREIGN KEY (`ustensile_id`) REFERENCES `ustensile`(`ustensile_id`);
-  ADD CONSTRAINT `fk_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
+  ADD CONSTRAINT `fk_ustensile_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
 ALTER TABLE `ingredient`
   ADD CONSTRAINT `pk_ingredient` PRIMARY KEY (`ingredient_id`);
@@ -62,7 +73,7 @@ ALTER TABLE `ingredient`
 ALTER TABLE `ingredient_recette`
   ADD CONSTRAINT `pk_ingredient_recette` PRIMARY KEY (`recette_id`,`ingredient_id`);
   ADD CONSTRAINT `fk_ingredient` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient`(`ingredient_id`);
-  ADD CONSTRAINT `fk_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
+  ADD CONSTRAINT `fk_ingredient_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
 ALTER TABLE `tag`
   ADD CONSTRAINT `pk_tag` PRIMARY KEY (`tag_id`);
@@ -72,5 +83,13 @@ ALTER TABLE `tag_recette`
   ADD CONSTRAINT `fk_tag` FOREIGN KEY (`tag_id`) REFERENCES `tag`(`tag_id`);
   ADD CONSTRAINT `fk_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
-ALTER TABLE `origine`
-  ADD CONSTRAINT `pk_origine` PRIMARY KEY (`origine_id`);
+
+
+describe recette;
+describe ustensile;
+describe ustensile_recette;
+describe ingredient;
+describe ingredient_recette;
+describe tag;
+describe tag_recette;
+describe origine;
