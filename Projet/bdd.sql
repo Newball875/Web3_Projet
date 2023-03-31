49e32b5dac7c0ae7bbace3d626a2757cbf116cd3
devisme valentin <vdevisme@poireauxl.kilimandjaro.calais.fr>
2023-03-31 16:04:01 +0200

    update bdd incomplete

--- Projet/bdd.sql
+++ Projet/bdd.sql
@@ -56,31 +56,31 @@ ALTER TABLE `origine`
   ADD CONSTRAINT `pk_origine` PRIMARY KEY (`origine_id`);
 
 ALTER TABLE `recette`
-  ADD CONSTRAINT `pk_recette` PRIMARY KEY (`recette_id`);
+  ADD CONSTRAINT `pk_recette` PRIMARY KEY (`recette_id`),
   ADD CONSTRAINT `fk_recette_origine` FOREIGN KEY (`origine_id`) REFERENCES `origine`(`origine_id`);
 
 ALTER TABLE `ustensile`
   ADD CONSTRAINT `pk_ustensile` PRIMARY KEY (`ustensile_id`);
 
 ALTER TABLE `ustensile_recette`
-  ADD CONSTRAINT `pk_ustensile_recette` PRIMARY KEY (`recette_id`,`ustensile_id`);
-  ADD CONSTRAINT `fk_ustensile` FOREIGN KEY (`ustensile_id`) REFERENCES `ustensile`(`ustensile_id`);
+  ADD CONSTRAINT `pk_ustensile_recette` PRIMARY KEY (`recette_id`,`ustensile_id`),
+  ADD CONSTRAINT `fk_ustensile` FOREIGN KEY (`ustensile_id`) REFERENCES `ustensile`(`ustensile_id`),
   ADD CONSTRAINT `fk_ustensile_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
 ALTER TABLE `ingredient`
   ADD CONSTRAINT `pk_ingredient` PRIMARY KEY (`ingredient_id`);
 
 ALTER TABLE `ingredient_recette`
-  ADD CONSTRAINT `pk_ingredient_recette` PRIMARY KEY (`recette_id`,`ingredient_id`);
-  ADD CONSTRAINT `fk_ingredient` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient`(`ingredient_id`);
+  ADD CONSTRAINT `pk_ingredient_recette` PRIMARY KEY (`recette_id`,`ingredient_id`),
+  ADD CONSTRAINT `fk_ingredient` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient`(`ingredient_id`),
   ADD CONSTRAINT `fk_ingredient_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
 ALTER TABLE `tag`
   ADD CONSTRAINT `pk_tag` PRIMARY KEY (`tag_id`);
 
 ALTER TABLE `tag_recette`
-  ADD CONSTRAINT `pk_tag_recette` PRIMARY KEY (`tag_id`,`recette_id`);
-  ADD CONSTRAINT `fk_tag` FOREIGN KEY (`tag_id`) REFERENCES `tag`(`tag_id`);
+  ADD CONSTRAINT `pk_tag_recette` PRIMARY KEY (`tag_id`,`recette_id`),
+  ADD CONSTRAINT `fk_tag` FOREIGN KEY (`tag_id`) REFERENCES `tag`(`tag_id`),
   ADD CONSTRAINT `fk_recette` FOREIGN KEY (`recette_id`) REFERENCES `recette`(`recette_id`);
 
 
