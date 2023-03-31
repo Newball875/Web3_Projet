83b3bc9445dd527b0fbfbef27a294bbb522efd27
capitaine robin <rcapitai@poireauxl.kilimandjaro.calais.fr>
2023-03-31 14:57:07 +0200

    SALUUT

---
+++ Projet/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food Culture</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+
+</head>
+<body>
+
+<?php include "class/header.html" ?>
+
+<div class="title"></div>
+<form id="accueil" method="POST" enctype="multipart/form-data">
+    <div class="neon">
+        <img src="images/dinosaur.jpg">
+        <div class="neon">
+            <label for="name" class="form-label">Nom</label>
+        </div>
+        <div class="neon">
+            <label for="description" class="form-label">Description</label>
+        </div>
+    </div>
+    <div class="neon">
+        <img src="images/dinosaur.jpg">
+        <div class="neon">
+            <label for="name" class="form-label">Nom</label>
+        </div>
+        <div class="neon">
+            <label for="description" class="form-label">Description</label>
+        </div>
+    </div>
+
+</form>
+<script>
+
+<?php include "class/footer.html" ?>
+
+</body>
+</html>
