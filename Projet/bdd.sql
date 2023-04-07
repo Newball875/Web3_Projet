e68609c21fe401ec3a9ec5074b3e9bf23e4541ef
devisme valentin <vdevisme@poireauxl.kilimandjaro.calais.fr>
2023-04-07 09:31:30 +0200

    bdd modif

--- Projet/bdd.sql
+++ Projet/bdd.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS recette;
 CREATE TABLE `recette` (
-  `recette_id` int NOT NULL AUTO_INCREMENT,
+  `recette_id` int UNIQUE NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `description` longtext,
   `instructions` longtext NOT NULL,
