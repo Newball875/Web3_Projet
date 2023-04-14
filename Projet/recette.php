2039b22fb09cfd9bb87327930b27c2640d27bb81
Newball <newball875@gmail.com>
2023-04-14 14:30:46 +0200

    Modification souci id

--- Projet/recette.php
+++ Projet/recette.php
@@ -1,24 +1,10 @@
 <?php
-$id=$_GET["id"];
+if(isset($_GET["id"])){
+    $id=$_GET["id"];
+}
 include "class/head.php";
 ?>
 
-<<<<<<< HEAD
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Food Culture</title>
-
-    <link rel="stylesheet" href="css/stylerecette.css">
-    <script src="javascript/stylerecette.js"></script>
-
-</head>
-=======
->>>>>>> d91e61c514d83c2a069302a0519eb03cb124db3b
 <body>
 <?php include "class/header.html" ?>
 
