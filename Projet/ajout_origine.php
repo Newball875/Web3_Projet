1430411ca81b39982e7a0ef6b776a7de7521125b
Newball <newball875@gmail.com>
2023-04-14 14:40:32 +0200

    Create ajout_origine.php

---
+++ Projet/ajout_origine.php
@@ -0,0 +1,21 @@
+<?php include_once "init.php" ?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food Culture</title>
+
+    <link rel="stylesheet" href="css/stylerecherche.css">
+    <script src="js/script.js"></script>
+
+</head>
+<body>
+
+<?php include "class/header.html" ?>
+
+
+
+<?php include "class/footer.php" ?>
