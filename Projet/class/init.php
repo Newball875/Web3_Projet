7c3386d0ff1d04eeecaae224a53f263f1f4ba0b8
Newball <newball875@gmail.com>
2023-04-14 14:36:13 +0200

    Ajout d'un init.php pour chaque fichier

---
+++ Projet/class/init.php
@@ -0,0 +1,6 @@
+<?php
+$path=getcwd().DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."autoloader.php";
+$chemin_image="ressources".DIRECTORY_SEPARATOR."img".DIRECTORY_SEPARATOR;
+require $path;
+Autoloader::register();
+$pdo=Connexion::connect(); ?>
