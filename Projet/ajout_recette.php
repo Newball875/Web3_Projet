8f17cf81f00e95bcf0425bfdce95868769e19710
capitaine robin <rcapitai@kilimandjaro.calais.fr>
2023-05-05 10:00:38 +0200

    ajout_recette.css

--- Projet/ajout_recette.php
+++ Projet/ajout_recette.php
@@ -27,46 +27,46 @@ $tab_origine=Connexion::prendreTousOrigines($pdo);
 </div>
 <div>
     <form action="update_recette.php" method="POST" enctype="multipart/form-data">
-	    <div id="infos">
-		    <label for="le_fichier" class="form_label">Uploader une recette :</label>
-		    <input type="text" class="form-control" id="name" name="name" placeholder="Nom de la recette">
+        <div id="infos">
+            <label for="le_fichier" class="form_label">Uploader une recette :</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Nom de la recette">
             <textarea id="instructions" name="instructions" rows="5" cols="33" placeholder="Instructions"></textarea>
-	    	<input type="file" class="form-control" id="image" name="image">
-	    </div>
+            <input type="file" class="form-control" id="image" name="image">
+        </div>
         <div id="origine">
             <label for="le_media" class="form_label" enctype="multipart/form-data">
-            
+
         </div>
         <div id="liste-ingredients">
             <h1>Ingrédients</h1>
             <div>
-            <select class="menu_ingredient" name="ingredients0">
-                <option value="" disabled>Ingrédients</option>
-                <?php
-                foreach($tab_ingredients as $ingredient){
-                    echo "<option value='$ingredient->id'>$ingredient->nom</option>";
-                }
-                ?>
-            </select>
-            <input type="number" class="form-control" name="quantite0">
+                <select class="menu_ingredient" name="ingredients0">
+                    <option value="" disabled>Ingrédients</option>
+                    <?php
+                    foreach($tab_ingredients as $ingredient){
+                        echo "<option value='$ingredient->id'>$ingredient->nom</option>";
+                    }
+                    ?>
+                </select>
+                <input type="number" class="form-control" name="quantite0">
             </div>
         </div>
         <div id="choix-origine">
             <h1>Origine</h1>
             <div>
-            <select class="origine" name="origine">
-                <option value="" disabled>Origine</option>
-                <?php
-                foreach($tab_origine as $media){
-                    echo "<option value='$media->id'>$media->nom</option>";
-                }
-                ?>
-            </select>
+                <select class="origine" name="origine">
+                    <option value="" disabled>Origine</option>
+                    <?php
+                    foreach($tab_origine as $media){
+                        echo "<option value='$media->id'>$media->nom</option>";
+                    }
+                    ?>
+                </select>
             </div>
         </div>
-	    <div id="bouton_final">
-		    <button type="submit" name="ok" value="1">Envoyer</button>
-    	</div>
+        <div id="bouton_final">
+            <button type="submit" name="ok" value="1">Envoyer</button>
+        </div>
     </form>
     <div id="boutons">
         <button id="moins">-</button>
