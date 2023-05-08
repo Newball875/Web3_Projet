3574599c946b4337de386328c4e059f488647986
Newball <newball875@gmail.com>
2023-05-08 11:18:06 +0200

    Modifications login

--- Projet/class/init.php
+++ Projet/class/init.php
@@ -3,4 +3,6 @@ $path=getcwd().DIRECTORY_SEPARATOR."class".DIRECTORY_SEPARATOR."autoloader.php";
 $chemin_image="ressources".DIRECTORY_SEPARATOR."img".DIRECTORY_SEPARATOR;
 require $path;
 Autoloader::register();
-$pdo=Connexion::connect(); ?>
+$pdo=Connexion::connect();
+session_start();
+?>
--- Projet/login.php
+++ Projet/login.php
@@ -1,11 +1,12 @@
+<?php include "class/init.php";?>
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inferface Login</title>
+    <title>Food Culture</title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -15,56 +16,59 @@
 </head>
 <body>
 
-<?php include "class/header.html" ?>
-
-<form id="login-form"
-      action="http://www.web.gregory-bourguin.fr/teaching/php/requests/ajax/00_test_REQ.php"
-      target="_blank"
-      method="post"
->
-
-    <h1 style="text-align: center">Menu Login</h1>
-
+<?php include "class/header.php";
+$login=0;
+$message="";
+if(isset($_SESSION["nick"]) and $_SESSION["nick"]=="recette"){
+    $login=2;
+}else{
+    if(isset($_POST["nick"])){
+        if($_POST["nick"]=="recette"){
+            if(isset($_POST["password"])){
+                if($_POST["password"]=="1234"){
+                    $login=1;
+                }else{
+                    $message="Mot de passe incorrect !";
+                }
+            }
+        }else{
+            $message="Pseudo incorrect !";
+        }
+    }
+}
+if($login==0){?>
+<form id="login-form" action="login.php" method="post">
+    <h1 style="text-align: center">Login</h1>
     <div id="content">
-
         <div id="inputs">
-
             <div class="form-group">
-                <label for="nick">Nickname</label>
-                <input type="text" class="form-control" name="nick" id="nick" value="didier">
+                <label for="nick">Pseudo</label>
+                <input type="text" class="form-control" name="nick" id="nick" value="recette">
             </div>
-
             <div class="form-group">
                 <label for="password">Mot de Passe</label>
-                <input type="text" class="form-control" name="password" id="password" value="1234">
-            </div>
-
-        </div>
-
-        <div id="preview">
-            <div id="preview-header">
-                <div id="preview-nick"></div>
-            </div>
-
-            <div id="preview-password">
-                <div id="preview-MDP"></div> &nbsp;
+                <input type="password" class="form-control" name="password" id="password" value="1234">
             </div>
         </div>
-
+        <p id="message-erreur"><?=$message?></p>
     </div>
-
     <div id="buttons">
         <button type="submit" class="envoyer">Envoyer</button>
         <div style="width: 1em"></div>
     </div>
-
 </form>
+<?php
+}else{
+    echo "HELLO";
+    if($login==1){
+        $_SESSION["nick"]=$_POST["nick"];
+    }?>
+    <button id="logout" onclick="window.location.href= 'unlog.php'">LOG OUT</button>
+    <?php
+}
+?>
 
-<div id="message" class="hidden error"></div>
 
-<div id="server-msg" style="margin-top: 20px"></div>
-
-<script src="javascript/login.js"></script>
 
 <?php include "class/footer.php" ?>
 
