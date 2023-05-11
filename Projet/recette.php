6b69346e4f7b89cc07dd54b3d92ae857908d495f
UnGarconSansCedille <robin.capitaine@gmail.com>
2023-05-11 21:27:30 +0200

    changement fin

--- Projet/recette.php
+++ Projet/recette.php
@@ -89,10 +89,12 @@ if(isset($_GET["id"])){
         <?php
         $results=Connexion::prendreOrigine($pdo,$id);
         echo "Origine : ".$results->nom;
+        echo "<div class='description_origine'>";
         echo "<div id='description'>";
         echo "<img src='$chemin_image"."/origine/"."$results->image' alt='$results->nom'>";
         echo "<p>$results->description</p>";
         ?>
+        </div>
         </div>
     </div>
 </form>
